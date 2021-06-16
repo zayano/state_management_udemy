6d991d3aa6b517a6e403ec11d56a8fed860c4ee9
Reza Aditya Sutrisno <adityareza46@gmail.com>
2021-06-16 16:56:22 +0700

    GetX 2 - Set Back Logic

--- lib/infrastructure/user/user_repository.dart
+++ lib/infrastructure/user/user_repository.dart
@@ -1,18 +1,21 @@
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
-import 'package:state_management_udemy/domain/core/user/model/user_response.dart';
+import 'package:state_management_udemy/domain/core/user/model/user_req_res.dart';
+import 'package:state_management_udemy/domain/core/user/user_data.dart';
 
-class ProfileRepository {
+class UserRepository {
   Dio _dio = Dio();
 
-  Future<Either<String, UserResponse>> getAllUserData() async {
+  Future<Either<String, UserData>> createNewUser(
+      RequestData requestData) async {
     Response _response;
     try {
-      _response = await _dio.get("https://reqres.in/api/users?page=2");
-      UserResponse _userResponse = UserResponse.fromJson(_response.data);
+      _response = await _dio.post("https://reqres.in/api/users",
+          data: requestData.toJson());
+      final _result = _response.data;
 
-      return right(_userResponse);
+      return right(UserData.fromJson(_result));
     } on DioError catch (e) {
       //error yang dihasilkan oleh dio
       print(e.response!.statusCode);
